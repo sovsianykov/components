5ab106462c0281eed93670c4d6f05dab0982f85b
applestock <swebproger@gmail.com>
2022-11-12 14:40:57 +0200

    first commit

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+
+const Home = () => {
+  return (
+    <Box
+      width="100%"
+      height="100vh"
+      display="flex"
+      sx={{ alignItems: "center", justifyContent: "center" }}
+    >
+      <Typography variant="h5">Commit 1</Typography>
+    </Box>
+  );
+};
+
+export default Home;
